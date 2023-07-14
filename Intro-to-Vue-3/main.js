61dadd42323498bcad2dc27a6ce98270723beda9
Carlos Eduardo <ipss.leiva@gmail.com>
2023-07-14 02:51:57 -0400

    Inicio de proyecto - intro-to-vue-3

---
+++ Intro-to-Vue-3/main.js
@@ -0,0 +1,17 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            // Solution
+            description: 'A warm fuzzy pair of socks.' ,
+            // Solution
+            precio: "10.001 CLP",
+            // Imagen 
+            image: './assets/images/socks_blue.jpg',
+
+            alt: 'Imagen foto de prueba socks blue',
+
+            url: '#'
+        }
+    }
+})
